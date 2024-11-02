10f832faaa7ec873d525897817cf6538473d0dd6
Antonio <dongyixiao2001@163.com>
2024-11-02 17:39:02 +0800

    add polyline plot methods

--- app/src/main/java/com/cyclequest/application/ui/component/map/MapPage.kt
+++ app/src/main/java/com/cyclequest/application/ui/component/map/MapPage.kt
@@ -13,13 +13,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
+import android.graphics.Color
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
-
+import com.amap.api.maps2d.AMap
 import com.amap.api.maps2d.CameraUpdateFactory
 import com.amap.api.maps2d.MapView
 import com.amap.api.maps2d.model.CameraPosition
 import com.amap.api.maps2d.model.LatLng
+import com.amap.api.maps2d.model.PolylineOptions
 
 @Composable
 fun MapPage(
@@ -37,27 +39,27 @@ fun MapPage(
     )
 }
 
-//@Composable
-//fun NavigationScreen(
-//    modifier: Modifier = Modifier,
-//    cameraPositionState: CameraPositionState = rememberCameraPositionState()
-//) {
-//    val mapView = rememberMapViewWithLifecycle()
-//
-//    AndroidView(
-//        modifier = modifier,
-//        factory = { mapView },
-//        update = { view ->
-//            view.map.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPositionState.position))
-//        }
-//    )
-//}
-
 @Composable
 fun rememberMapViewWithLifecycle(): MapView {
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
     val mapView = remember { MapView(context) }
+
+//    Map Control Object
+    var mAMap: AMap = mapView.map
+
+//    Test: polyline
+    val latLngs: MutableList<LatLng> = ArrayList()
+    latLngs.add(LatLng(39.999391, 116.135972))
+    latLngs.add(LatLng(39.898323, 116.057694))
+    latLngs.add(LatLng(39.900430, 116.265061))
+    latLngs.add(LatLng(39.955192, 116.140092))
+    mAMap.addPolyline(
+        PolylineOptions().addAll
+            (latLngs).width(10f).color(Color.argb(255, 1, 1, 1))
+    )
+
+//    TODO: 引入DiscoveryLayer与RoutingLayer，需要传入mAMap变量，控制自定义图层绘制
 
     DisposableEffect(lifecycleOwner) {
         val observer = LifecycleEventObserver { _, event ->
